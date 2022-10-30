3f83ed89d65690c194323d15fc5d976dad6d7008
Gabriele Fiorentino <gabrielefiorentino2@gmail.com>
2022-10-30 15:53:15 +0100

    End puzzle prototype

--- MuseumGame/Assets/Scripts/PuzzleScripts/DragController.cs
+++ MuseumGame/Assets/Scripts/PuzzleScripts/DragController.cs
@@ -1,26 +1,31 @@
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Serialization;
 
 public class DragController : MonoBehaviour
 {
-    public GameObject _selectedPiace;
-    private int c = 0;
+    [FormerlySerializedAs("_selectedPiace")] public GameObject selectedPiace;
+    private int c = 0, OIL = 1;
+    private Touch _touch;
     private void Update()
     {
-        // if (Input.touchCount > 0) {
-        //     
-        //     RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position), Vector2.zero);
-        //     if (hit.transform.CompareTag("Puzzle"))
-        //     {
-        //         if (!hit.transform.GetComponent<PieceManager>().inRhightPosition)
-        //         {
-        //             _selectedPiace = hit.transform.gameObject;
-        //             _selectedPiace.GetComponent<PieceManager>().selected = true;
-        //         }
-        //        
-        //     }
-        //     c = 1;
-        //     
-        // }else 
+        DetectTouch();
+        
+        if (_touch.phase == TouchPhase.Began) {
+            
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(_touch.position), Vector2.zero);
+            if (hit.transform.CompareTag("Puzzle"))
+            {
+                if (!hit.transform.GetComponent<PieceManager>().inRhightPosition)
+                {
+                    selectedPiace = hit.transform.gameObject;
+                    selectedPiace.GetComponent<PieceManager>().selected = true;
+                }
+            }
+            c = 1;
+            
+            //Input.GetTouch(0).position
+        }else 
         if (Input.GetMouseButtonDown(0)) {
             
             RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
@@ -28,45 +33,62 @@ public class DragController : MonoBehaviour
             {
                 if (!hit.transform.GetComponent<PieceManager>().inRhightPosition)
                 {
-                    _selectedPiace = hit.transform.gameObject;
-                    _selectedPiace.GetComponent<PieceManager>().selected = true;
+                    selectedPiace = hit.transform.gameObject;
+                    selectedPiace.GetComponent<PieceManager>().selected = true;
+                    selectedPiace.GetComponent<SortingGroup>().sortingOrder = OIL;
+                    OIL++;
                 }
             }
             c = 2;
             
         }
+        
+        
 
         switch (c)
         {
             case 1: 
-                if (Input.touchCount == 0)
+                if (_touch.phase == TouchPhase.Ended)
                 {
-                    _selectedPiace.GetComponent<PieceManager>().selected = false;
-                    _selectedPiace = null;
+                    if (selectedPiace != null)
+                    {
+                        selectedPiace.GetComponent<PieceManager>().selected = false;
+                        selectedPiace = null;
+                    }
                 }
                 
-                if (_selectedPiace != null)
+                if (selectedPiace != null)
                 {
-                    Vector3 _touchPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-                    _selectedPiace.transform.position = new Vector3(_touchPos.x,_touchPos.y,0);
+                    Vector3 touchPos = Camera.main.ScreenToWorldPoint(_touch.position);
+                    selectedPiace.transform.position = new Vector3(touchPos.x, touchPos.y,0);
                 }
                 break;
             
             case 2:
-                
                 if (Input.GetMouseButtonUp(0))
                 {
-                    _selectedPiace.GetComponent<PieceManager>().selected = false;
-                    _selectedPiace = null;
+                    if (selectedPiace != null)
+                    {
+                        selectedPiace.GetComponent<PieceManager>().selected = false;
+                        selectedPiace = null;
+                    }
                 }
                 
-                if (_selectedPiace != null)
+                if (selectedPiace != null)
                 {
-                    Vector3 _mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                    _selectedPiace.transform.position = new Vector3(_mousePos.x, _mousePos.y,0);
+                    Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    selectedPiace.transform.position = new Vector3(mousePos.x, mousePos.y,0);
                 }
                 break;
         }
         
+    }
+
+    private void DetectTouch()
+    {
+        if (Input.touchCount > 0)
+        {
+            _touch = Input.GetTouch(0);
+        }
     }
 }
--- MuseumGame/Assets/Scripts/PuzzleScripts/PieceManager.cs
+++ MuseumGame/Assets/Scripts/PuzzleScripts/PieceManager.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PieceManager : MonoBehaviour
 {
-    private Vector3 _rightPosition;
+    Vector3 _rightPosition;
     public bool inRhightPosition;
     public bool selected;
+    private Renderer _pieceRenderer;
+    private int _rightPositionPiece = 0;
     
     // Start is called before the first frame update
     void Start()
     {
+        _pieceRenderer = new Renderer();
         _rightPosition = transform.position;
         transform.position = new Vector3(Random.Range(2.4f,7.5f),Random.Range(9.7f,5.5f));
+        Debug.Log(_rightPosition);
     }
 
     // Update is called once per frame
@@ -24,7 +29,19 @@ public class PieceManager : MonoBehaviour
             {
                 transform.position = _rightPosition;
                 inRhightPosition = true;
+                _pieceRenderer.sortingOrder = 0; 
+
             }
+        }
+    }
+
+    private void IncrementRightPositionPiece()
+    {
+        _rightPositionPiece++;
+
+        if (_rightPositionPiece >= 36)
+        {
+            
         }
     }
 }
